97116fcd4df3191417b68285fdf9850cd9f244f4
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-01-30 03:29:41 +0530

    fix: errors of craco, purging & overwriting react

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+// page container
+const PageContainer = styled.div `
+
+`;
+
+export function HomePage() {
+    return <PageContainer>
+        Hello WOrld Hirva!
+    </PageContainer>
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkmode: false,
   content: [],
   theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
