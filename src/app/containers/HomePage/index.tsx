7b2587115f9e0d83cd1ffb3ee37ebc402bfefd49
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-02-08 00:00:39 +0530

    step1: understading work with tailwind in react

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import tw from "twin.macro";
 
 // page container
 const PageContainer = styled.div `
-
+    ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden
+    `}
 `;
 
 export function HomePage() {
     return <PageContainer>
-        Hello WOrld Hirva!
+        Hello to World from Hirva!
     </PageContainer>
 }
