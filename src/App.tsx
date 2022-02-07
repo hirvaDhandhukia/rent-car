cdb33a0e48b5190878db0af682c4e1e6784a0e26
Hirva Dhandhukia <hsdhandhukia@gmail.com>
2022-02-07 23:09:14 +0530

    edit: readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import { HomePage } from './app/containers/HomePage';
 
 function App() {
   return (
     <div className="App">
       Hello World!
+      <HomePage />
     </div>
   );
 }
