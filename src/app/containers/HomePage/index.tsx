c9688677a516069bbb859f46a15298ddcee265f8
Hirva Dhandhukia <hirva.dhandhukia@gmail.com>
2023-03-09 01:30:39 +0530

    add: landing sect

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 import { Navbar } from "../../components/navbar";
+import { TopSection } from "./tepSection";
 
 const PageContainer = styled.div`
     ${tw`
@@ -17,5 +18,6 @@ const PageContainer = styled.div`
 export function HomePage() {
     return <PageContainer>
         <Navbar />
+        <TopSection />
     </PageContainer>
 }
