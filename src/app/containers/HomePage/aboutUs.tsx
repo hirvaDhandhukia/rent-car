5c8add56267a13c0a8e17f6660e2c47b7095602e
Hirva Dhandhukia <hirva.dhandhukia@gmail.com>
2023-05-04 00:03:45 +0530

    fix: description

--- src/app/containers/HomePage/aboutUs.tsx
+++ src/app/containers/HomePage/aboutUs.tsx
@@ -85,7 +85,6 @@ export function AboutUs() {
                 <Title>Feel The Best Experience With Our Rental Deals</Title>
                 <InfoText>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Utenim adminim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiatnulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
                 </InfoText>
             </InfoContainer>
         </AboutUsContainer>
